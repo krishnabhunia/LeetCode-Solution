afef431f1be4124b3064f54437f54572694cb50e
KRISHNA DIPAYAN BHUNIA <23618035+krishnabhunia@users.noreply.github.com>
2023-09-02 16:44:00 +0530

    leetcode

---
+++ 69.sqrt-x.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+import math
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        print(math.sqrt(x))
+        return math.sqrt(x)
+        
+# @lc code=end
+
